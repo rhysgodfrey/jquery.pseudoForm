ca36a8eab7c222d131647e803e5224ccf448cf25
Rhys Godfrey <rhys.godfrey@gmail.com>
2012-05-05 21:53:23 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
-jquery.pseudoForm
-=================
+﻿jquery.pseudoForm
+=======================================
 
-Jquery plugin to form submission inputs to be scoped to an element
+## Introduction ##
+
+ASP.Net web forms limit developers to one form per page. There are techniques to work around this when
+required, in particular the use of the PostBackUrl attribute on button controls. This works well, but all
+inputs on the form are posted to the PostBackUrl, this plugin helps limit the form fields that are posted
+to the target URL.
+
+As this is a jQuery plugin this is not limited to ASP.Net web form sites, but in any similar situation.
+
+## Usage ##
+
+Copy the jquery.pseudoForm javascript file into your site, and add a reference to it on to your page,
+after referencing jQuery:
+
+``` html
+<script type="text/javascript" src="/scripts/jquery.pseudoForm.js"></script>
+```
+
+Wrap the form elements you wish to post together in a div (or any other HTML element), for example
+
+``` html
+<form action="/test.aspx" method="POST" id="mainForm" name="mainForm">
+    <div id="formAreaOne">
+	    <input type="hidden" name="var1" id="var1" value="one" />
+		<input type="text" name="var2" id="var2" value="two" />
+		<textarea id="var3" name="var3"></textarea>
+		<input type="submit" name="button1" id="button1" value="Button One" />
+		<input type="submit" name="button2" id="button2" value="Button Two" />
+	</div>
+
+	<div id="formAreaTwo">
+	    <input type="text" name="var4" id="var4" value="four" />
+	    <input type="hidden" name="var5" id="var5" value="five" />
+		<input type="submit" name="button3" id="button3" value="Button Three" />
+	</div>
+</form>
+```
+
+Currently clicking any of the 3 buttons will cause all form fields (var1-5) to be posted to test.aspx.
+
+The pseudoForm jQuery plugin allows you to turn one of the div's into a standalone form, e.g. only posting
+var4 and var5 after clicking button3:
+
+``` html
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#formAreaTwo').pseudoForm();
+    });
+</script>
+```
+
+The same can be applied to formAreaOne.
+
+If you would like one of the 'forms' to post to a different URL specify the postbackUrl setting as below:
+
+``` html
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#formAreaTwo').pseudoForm({ postbackUrl : '/OtherPage.aspx' });
+    });
+</script>
+```
+
+## License ##
+
+This plugin is released under the *FreeBSD License*, see **LICENSE.txt** for more information.
+
+Blog: http://www.rhysgodfrey.co.uk
+Twitter: @rhysgodfrey
---
+++ jquery.pseudoform.js
@@ -0,0 +1,90 @@
+﻿// jQuery plugin to scope form postbacks https://github.com/rhysgodfrey/jquery.pseudoForm
+// version 1.0, 5th May 2012 by Rhys Godfrey (http://www.rhysgodfrey.co.uk)
+// This plugin is released under the FreeBSD License, see https://github.com/rhysgodfrey/jquery.pseudoForm/blob/master/License.txt for more information
+
+// Based on jQuery Plugin Boilerplate version 1.1, May 14th, 2011 by Stefan Gabos
+
+(function ($) {
+
+    // here we go!
+    $.pseudoForm = function (element, options) {
+
+        // plugin's default options
+        // this is private property and is  accessible only from inside the plugin
+        var defaults = {
+            postbackUrl: ''
+
+        }
+
+        // to avoid confusions, use "plugin" to reference the
+        // current instance of the object
+        var plugin = this;
+
+        // this will hold the merged default, and user-provided options
+        // plugin's properties will be available through this object like:
+        // plugin.settings.propertyName from inside the plugin or
+        // element.data('pseudoForm').settings.propertyName from outside the plugin,
+        // where "element" is the element the plugin is attached to;
+        plugin.settings = {}
+
+        var $element = $(element), // reference to the jQuery version of DOM element
+             element = element;    // reference to the actual DOM element
+
+        // the "constructor" method that gets called when the object is created
+        plugin.init = function () {
+
+            // the plugin's final properties are the merged default and
+            // user-provided options (if any)
+            plugin.settings = $.extend({}, defaults, options);
+
+            var self = this;
+
+            $element.children(':submit').each(function () {
+                $(this).click(function () {
+                    var jqForm = $element.parents('form');
+
+                    var externalnputs = jqForm.find(':input').not($element.find(':input'));
+                    externalnputs.attr('disabled', 'disabled');
+
+                    if (self.settings.postbackUrl != '') {
+                        var form = jqForm.get(0);
+
+                        form.action = self.settings.postbackUrl;
+                    }
+                });
+            });
+        }
+
+        // fire up the plugin!
+        // call the "constructor" method
+        plugin.init();
+
+    }
+
+    // add the plugin to the jQuery.fn object
+    $.fn.pseudoForm = function (options) {
+
+        // iterate through the DOM elements we are attaching the plugin to
+        return this.each(function () {
+
+            // if plugin has not already been attached to the element
+            if (undefined == $(this).data('pseudoForm')) {
+
+                // create a new instance of the plugin
+                // pass the DOM element and the user-provided options as arguments
+                var plugin = new $.pseudoForm(this, options);
+
+                // in the jQuery version of the element
+                // store a reference to the plugin object
+                // you can later access the plugin and its methods and properties like
+                // element.data('pseudoForm').publicMethod(arg1, arg2, ... argn) or
+                // element.data('pseudoForm').settings.propertyName
+                $(this).data('pseudoForm', plugin);
+
+            }
+
+        });
+
+    }
+
+})(jQuery);
